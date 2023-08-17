2cd3846172fdc41349ec303a1612e644ba312b0e
ipekdural <duralipek6@gmail.com>
2023-08-17 03:02:12 +0300

    -

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+choice=input("Type 'y' to get another card , type 'n' to pass.").lower()
+# if choice=='y':
+#     deal_
---
+++ test.py
@@ -0,0 +1,9 @@
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+user_cards = []
+computer_cards = []
+
+ 
+print("User Cards:", user_cards)
+print("Computer Cards:", computer_cards)
