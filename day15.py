eee1ce8e5b9796e492be8e260447619a93b37945
ipekdural <duralipek6@gmail.com>
2023-08-22 00:44:55 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ day15.py
@@ -0,0 +1,86 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+is_on = True
+profit = 0
+
+
+def is_resource_succifient(ingredients):
+    for item in ingredients:
+        if ingredients[item] > resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+        return True
+
+
+def process_coins():
+    print("Please insert coins.")
+    total = int(input("How many quarters? ")) * 0.25
+    total += int(input("How many dimes?")) * 0.1
+    total += int(input("How many nickles?")) * 0.05
+    total += int(input("How many pennies?")) * 0.01
+    return total
+
+
+def is_money_enough(user_money, cost):
+    if user_money >= cost:
+        change = round(user_money - cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+def make_coffy(_choice, ingredients):
+    for item in ingredients:
+        resources[item] -= ingredients[item]
+    print(f"Here is your {_choice} ☕️. Enjoy!")
+
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']} ml")
+        print(f"Milk: {resources['milk']} ml")
+        print(f"Coffee: {resources['coffee']} ml")
+        print(f"Money: {profit}$")
+    else:
+        drink = MENU[choice]
+        if is_resource_succifient(drink["ingredients"]):
+            payment = process_coins()
+            if is_money_enough(payment, drink["cost"]):
+                make_coffy(choice, drink["ingredients"])
